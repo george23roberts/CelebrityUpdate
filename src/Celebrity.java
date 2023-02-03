cdf40fa9228a83460efa8bf3b164a45d442a2594
George Roberts (Class of 2023) <231660gr@internal.salesianum.org>
2023-02-03 14:09:54 -0500

    create

---
+++ src/Celebrity.java
@@ -0,0 +1,47 @@
+public class Celebrity{
+    
+    private int age;
+    private int height;
+    private String hairColor;
+    private String gender;
+    private String race;
+    private String weight;
+    private String job;
+    
+    
+    
+    public int getAge(){
+        return age;
+    }
+    
+    
+    public int getHeight(){
+        return height;
+    }
+    
+    public String getHairColor(){
+        return hairColor;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public String race(){
+        return race;
+    }
+    public String weight(){
+        return weight;
+    }
+    public String getJob(){
+        return job;
+    }
+    
+
+    
+    
+    
+    public static void main(String[] args) throws Exception {
+       
+
+
+    }
+}
