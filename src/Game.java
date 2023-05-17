c53eca654052b2d882cda7ce41c6173b7564ea38
George Roberts (Class of 2023) <231660gr@internal.salesianum.org>
2023-05-17 09:03:28 -0400

    Borderline Complete

--- src/Game.java
+++ src/Game.java
@@ -12,31 +12,126 @@ public class Game {
 
    List<Celebrity> ca;
    ca = new ArrayList<Celebrity>();
-   //name(ca);
+   celeb(ca);
    Random g = new Random();
    int randomNum = g.nextInt(ca.size());
    Celebrity picked = ca.get (randomNum);   
-   boolean correctguess = false;
-   while(correctguess = false) {
+   boolean correctGuess = false;
+   while(correctGuess = false) {
       Scanner guessReader = new Scanner(System.in);
-      System.out.println("Choose What to go for");
+      System.out.println("Guess The Celebrity");
+
+      String guess = guessReader.nextLine();
+
+
+
+         if(guess.equals(picked.getName())) {
+               correctGuess = true;
+         }
+
+         else {
+            Scanner attributePicker = new Scanner(System.in);
+
+            System.out.print("Pick An Attribute! -" + "Age\nHeight\nHairColor\nGender\nRace\nWeight\nOcccupationProffesionJob");
+
+            
+         }
       
+         
+
+         
+         
+
+         
+         int attributePicker2 = attributePicker.nextInt();
+         if (attributePicker2 == "Age") {
+         System.out.println("Take A Guess!");
+         String attempt = guessReader.nextLine();
+         if (attempt.equals(chosen.getAge())) {
+             System.out.println("Correct");
+         }
+         else{
+                 System.out.println("Wrong");
+             }
+         }
+         
+         if (attributePicker2 == "Height") {
+
+            System.out.println("Take A Guess!");
+         String attempt = s1.nextLine();
+         if (attempt.equals(chosen.getHeight())) {
+             System.out.println("Correct");
+         }
+         else{
+                 System.out.println("Wrong");
+             }
+         }
+
+         if (attributePicker2 == "HairColor")
+         System.out.println("Take A Guess!");
+         String attempt = s1.nextLine();
+             if (attempt.equals(chosen.getHairColor())) {
+                 System.out.println("Correct");
+             }
+             else{
+                 System.out.println("Wrong");
+             }
+         }
+         if (attributePicker2 == "Gender") {
+         System.out.println("Take A Guess!");
+         int attempt = s1.nextInt();
+             if (attempt.equals(chosen.getGender())) {
+                 System.out.println("Correct");
+             }
+             else{
+                 System.out.println("Wrong");
+             }
+         }
+         if (attributePicker2 == "Race") {
+            System.out.println("Take A Guess!");
+            int attempt = s1.nextInt();
+                if (attempt.equals(chosen.getRace())) {
+                    System.out.println("Correct");
+                }
+                else{
+                    System.out.println("Wrong");
+                }
+         }
+         if (attributePicker2 == "Weight") {
+            System.out.println("Take A Guess!");
+            int attempt = s1.nextInt();
+                  if (attempt.equals(chosen.getWeight())) {
+                       System.out.println("Correct");
+                   }
+                  else{
+                       System.out.println("Wrong");
+                   }
+         }
+         if (attributePicker2 == "OcccupationProffesionJob") {
+            System.out.println("Take A Guess!");
+            int attempt = s1.nextInt();
+                  if (attempt.equals(chosen.getJon())) {
+                       System.out.println("Correct");
+                   }
+                  else{
+                       System.out.println("Wrong");
+                   }
+         }
+   }  }                         
    }
    
-   public void name(ArrayList p ) {
-    p.add(p); Celebrity LeBronJames = new Celebrity(38, "6ft9", "black", "male", "black", 250, "NBA"); 
-    p.add(p); Celebrity MarkWinchell = new Celebrity(1900, "4ft11", "grey", "male", "white", 50, "Admissions");
-    p.add(p); Celebrity JonahHill = new Celebrity(39, "5ft7", "brown", "male", "white", 192, "Actor");
+   public static void celeb(List<Celebrity> celebs) {
+   Celebrity LeBronJames = new Celebrity(38, "6ft9", "black", "male", "black", 250, "NBA"); 
+   Celebrity MarkWinchell = new Celebrity(1900, "4ft11", "grey", "male", "white", 50, "Admissions");
+   Celebrity JonahHill = new Celebrity(39, "5ft7", "brown", "male", "white", 192, "Actor");
+   celebs.add(0, LeBronJames);
+   celebs.add(1, MarkWinchell);
+   celebs.add(2, JonahHill);
    }
+}
     
         
     
-}
-}
-
-
-
-
 
 
 
